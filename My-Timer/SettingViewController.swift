bc4fb0ea1b189c62fa48312efedd1b224e60eb8e
yukimasa ikeda <ykms123456789@gmail.com>
2019-07-20 19:44:12 +0900

    Timer function completed

--- My-Timer/SettingViewController.swift
+++ My-Timer/SettingViewController.swift
@@ -8,12 +8,33 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    // UIPickerViewに表示するでデータをArrayで作成
+    let settingArray : [Int] = [10, 20, 30, 40, 50, 60]
+    
+    // 設定値を覚えるキーを設定
+    let settingKey = "timer_value"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // timerSettingPickerのデリゲートとデータソースの通知先を指定
+        timerSettingPicker.delegate = self
+        timerSettingPicker.dataSource = self
+        
+        // UserDefaultsの取得
+        let settings = UserDefaults.standard
+        let timerValue = settings.integer(forKey: settingKey)
+        
+        // Pickerの選択を合わせる
+        for row in 0..<settingArray.count {
+            if settingArray[row] == timerValue {
+                timerSettingPicker.selectRow(row, inComponent: 0, animated: true)
+            }
+        }
     }
     
 
@@ -30,5 +51,31 @@ class SettingViewController: UIViewController {
     @IBOutlet weak var timerSettingPicker: UIPickerView!
     
     @IBAction func decisionButtonAction(_ sender: Any) {
+        // 前の画面に戻る
+        _ = navigationController?.popViewController(animated: true)
     }
+    
+    // UIPickerViewの列数を設定
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // UIPickerViewの行数を設定
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return settingArray.count
+    }
+    
+    // UIPickerViewの表示する内容を設定
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(settingArray[row])
+    }
+    
+    // picker選択時に実行
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // UserDefaultsの設定
+        let settings = UserDefaults.standard
+        settings.setValue(settingArray[row], forKey: settingKey)
+        settings.synchronize()
+    }
+    
 }
--- My-Timer/ViewController.swift
+++ My-Timer/ViewController.swift
@@ -9,21 +9,106 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // タイマーの変数を作成
+    var timer : Timer?
+    
+    // カウント（経過時間）の変数を作成
+    var count = 0
+    
+    // 設定値を扱うキーを設定
+    let settingKey = "timer_value"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // UserDefaultsのインスタンスを作成
+        let settings = UserDefaults.standard
+        
+        // UserDefaultsに初期設定を登録
+        settings.register(defaults: [settingKey:10])
     }
 
     @IBOutlet weak var countDownLabel: UILabel!
     
     @IBAction func settingButtonAction(_ sender: Any) {
+        // timerをアンラップしてnowTimerに代入
+        if let nowTimer = timer {
+            // もしタイマーが、実行中だったら停止
+            if nowTimer.isValid == true {
+                // タイマー停止
+                nowTimer.invalidate()
+            }
+        }
+        // 画面推移を行う
+        performSegue(withIdentifier: "goSetting", sender: nil)
     }
     
     @IBAction func startButtonAction(_ sender: Any) {
+        // timerをアンラップしてnowTimerに代入
+        if let nowTimer = timer {
+            // もしタイマーが、実行中だったらスタートしない
+            if nowTimer.isValid == true {
+                // 何も処理しない
+                return
+            }
+        }
+        
+        // タイマーをスタート
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                     target: self,
+                                     selector: #selector(self.timerInterrupt(_:)),
+                                     userInfo: nil,
+                                     repeats: true)
     }
     
     @IBAction func stopButtonAction(_ sender: Any) {
+        // timerをアンラップしてnowTimerに代入
+        if let nowTimer = timer {
+            // もしタイマーが、実行中だったら停止
+            if nowTimer.isValid == true {
+                // タイマー停止
+                nowTimer.invalidate()
+            }
+        }
+    }
+    
+    // 画面の更新をする（戻り値：remaincount：残り時間）
+    func displayUpdate() -> Int {
+        
+        // UserDefaultsのインスタンスを生成
+        let settings = UserDefaults.standard
+        // 取得した秒数をtimerValueに渡す
+        let timerValue = settings.integer(forKey: settingKey)
+        // 残り時間（remainCount）を生成
+        let remainCount = timerValue - count
+        // remainCount（残りの時間）をラベルに表示
+        countDownLabel.text = "残り\(remainCount)秒"
+        // 残り時間を戻り値に設定
+        return remainCount
+        
+    }
+    
+    // 経過時間の処理
+    @objc func timerInterrupt(_ timer:Timer) {
+        // count（経過時間）に+1していく
+        count += 1
+        //remainCount（残り時間）が0以下のとき、タイマーを止める
+        if displayUpdate() <= 0 {
+            // 初期化処理
+            count = 0
+            // タイマー停止
+            timer.invalidate()
+        }
+    }
+    
+    // 画面切り替えのタイミングで処理を行う
+    override func viewDidAppear(_ animated: Bool) {
+        // カウント（経過時間）をゼロにする
+        count = 0
+        // タイマーの表示を更新する
+        _ = displayUpdate()
     }
     
 }
